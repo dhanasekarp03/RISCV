6389e62ea005d70f1af44ae674566a75055d6f7a
Shreyas T <126799964+Shreyast27@users.noreply.github.com>
2024-03-21 21:03:32 +0530

    Update IFU.v

--- rtl/IFU.v
+++ rtl/IFU.v
@@ -1,30 +1,26 @@
-/*
-The instruction fetch unit has clock and reset pins as input and 32-bit instruction code as output.
-Internally the block has Instruction Memory, Program Counter(P.C) and an adder to increment counter by 4, 
-on every positive clock edge.
-*/
 `include "INST_MEM.v"
 
 module IFU(
-    input clock,reset,
-    output [31:0] Instruction_Code
+    input clock, 
+    input reset, 
+    output reg [31:0] Instruction_Code
 );
-reg [31:0] PC = 32'b0;  // 32-bit program counter is initialized to zero
-reg [31:0]count=0;
-    // Initializing the instruction memory block
-  INST_MEM instr_mem(PC,reset,clock,Instruction_Code);
+    reg [31:0] PC = 32'b0; // Initialize program counter to zero
+    reg [31:0] count = 0; // Initialize count variable
 
-  always @( posedge clock,posedge reset)
+    INST_MEM instr_mem(PC, reset, clock, Instruction_Code); // Instantiate Instruction Memory block
+
+    always @(posedge clock or posedge reset)
     begin
-        if(reset == 1)  //If reset is one, clear the program counter
-        PC <= 0;
-        else if(count>0)
-        PC<=PC+4;
+        if (reset == 1)
+            PC <= 0; // Clear program counter on reset
+        else if (count > 0)
+            PC <= PC + 4; // Increment program counter by 4
         else
         begin
-        PC <= PC;
-        count<=count+1;
-        end// Increment program counter on positive clock edge
+            PC <= PC; // Maintain program counter
+            count <= count + 1; // Increment count
+        end
     end
 
 endmodule
