61f6444fbda168e0d47da00db467eb6740474e18
Shreyas T <126799964+Shreyast27@users.noreply.github.com>
2024-03-21 21:07:50 +0530

    Update PROCESSOR.v

--- rtl/PROCESSOR.v
+++ rtl/PROCESSOR.v
@@ -3,19 +3,22 @@
 `include "IFU.v"
 
 module PROCESSOR( 
-    input clock, 
+    input clock,
     input reset,
-    output zero
+    output reg zero
 );
 
     wire [31:0] instruction_code;
     wire [3:0] alu_control;
     wire regwrite;
 
+    // Instantiate Instruction Fetch Unit
     IFU IFU_module(clock, reset, instruction_code);
-	
-    CONTROL control_module(instruction_code[31:25], instruction_code[14:12],instruction_code[6:0], alu_control, regwrite);
-	
+
+    // Instantiate Control Unit
+    CONTROL control_module(instruction_code[31:25], instruction_code[14:12], instruction_code[6:0], alu_control, regwrite);
+
+    // Instantiate Data Path Unit
     DATAPATH datapath_module(instruction_code[19:15], instruction_code[24:20], instruction_code[11:7], alu_control, regwrite, clock, reset, zero);
 
 endmodule
