f91b4348b7f93b84bf2fc8c939aba8533a6d016c
Shreyas T <126799964+Shreyast27@users.noreply.github.com>
2024-03-21 20:56:37 +0530

    Update CONTROL.v

--- rtl/CONTROL.v
+++ rtl/CONTROL.v
@@ -1,34 +1,30 @@
 module CONTROL(
-    input [6:0] funct7,
-    input [2:0] funct3,
-    input [6:0] opcode,
-    output reg [3:0] alu_control,
-    output reg regwrite_control
+       input [6:0] funct7,
+       input [2:0] funct3,
+       input [6:0] opcode,
+       output reg [3:0] alu_control,
+       output reg regwrite_control
 );
-    always @(funct3 or funct7 or opcode)
-    begin
-        if (opcode == 7'b0110011) begin // R-type instructions
+   always @(funct3 or funct7 or opcode)
+   begin
+       if (opcode == 7'b0110011) begin // Checking for R-type instructions
 
-            regwrite_control = 1;
+           regwrite_control = 1; // Setting register write control
 
-            case (funct3)
-                0: begin
-                    if(funct7 == 0)
-                    alu_control = 4'b0000; // ADD
-                    else if(funct7 == 32)
-                    alu_control = 4'b0001; // SUB
-                    end
-              
-                6: alu_control = 4'b0010; // logical OR
-                7: alu_control = 4'b0011; // logicalAND
-                4: alu_control = 4'b0100; // XOR
-				2: alu_control = 4'b0101; // SLT
-                
+           case (funct3)
+               0: begin
+                   if (funct7 == 0)
+                       alu_control = 4'b0000; // Setting ALU control for ADD
+                   else if (funct7 == 32)
+                       alu_control = 4'b0001; // Setting ALU control for SUB
+               end
              
-            endcase
+               6: alu_control = 4'b0010; // Setting ALU control for logical OR
+               7: alu_control = 4'b0011; // Setting ALU control for logical AND
+               4: alu_control = 4'b0100; // Setting ALU control for XOR
+               2: alu_control = 4'b0101; // Setting ALU control for SLT
+           endcase
 
-      end
-
-    end
-
+       end
+   end
 endmodule
