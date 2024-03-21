3d3789e7adbca4fd0be0c1c663d075ad2b571206
Shreyas T <126799964+Shreyast27@users.noreply.github.com>
2024-03-21 20:53:07 +0530

    Update ALU.v

--- rtl/ALU.v
+++ rtl/ALU.v
@@ -1,45 +1,24 @@
-/*
-ALU module, which takes two operands of size 32-bits each and a 4-bit ALU_control as input.
-Operation is performed on the basis of ALU_control value and output is 32-bit ALU_result. 
-If the ALU_result is zero, a ZERO FLAG is set.
-*/
-
-/*
-ALU Control lines | Function
------------------------------
-        0000    add
-        0001    sub
-        0010	or
-        0100	and
-        1000	xor
-        0011    set less than
-       
-*/
-
 module ALU (
-    input [31:0] in1,in2, 
-    input[3:0] alu_control,
-    output reg [31:0] alu_result,
-    output reg zero_flag
+    input [31:0] in1, in2,  // Input operands
+    input [3:0] alu_control, // ALU control lines
+    output reg [31:0] alu_result, // Result of ALU operation
+    output reg zero_flag // Flag indicating if result is zero
 );
     always @(*)
     begin
         // Operating based on control input
         case(alu_control)
-
-        4'b0000: alu_result = in1+in2;
-        4'b0001: alu_result = in1-in2;
-        4'b0010: alu_result = in1 || in2;
-        4'b0011: alu_result = in1 &&in2;
-        4'b0100: alu_result = in1^in2;
-        4'b0101: begin 
-            if(in1<in2)
-            alu_result = 1;
-            else
-            alu_result = 0;
-        end
-          
-
+            4'b0000: alu_result = in1 + in2; // Addition
+            4'b0001: alu_result = in1 - in2; // Subtraction
+            4'b0010: alu_result = in1 || in2; // Logical OR
+            4'b0011: alu_result = in1 && in2; // Logical AND
+            4'b0100: alu_result = in1 ^ in2; // XOR
+            4'b0101: begin 
+                if (in1 < in2) // Set on less than
+                    alu_result = 1; 
+                else
+                    alu_result = 0;
+            end
         endcase
 
         // Setting Zero_flag if ALU_result is zero
@@ -47,6 +26,5 @@ module ALU (
             zero_flag = 1'b1;
         else
             zero_flag = 1'b0;
-        
     end
 endmodule
